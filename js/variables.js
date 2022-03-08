97454d2af1bcf91de1364aef2979730c8d2cd4bf
GonzaJag <gonzalogodoy22@outlook.com>
2022-03-08 19:15:53 -0300

    cambios

--- js/variables.js
+++ js/variables.js
@@ -2,3 +2,5 @@ let m2 = 0
 let zona = 0
 const valorMetro2Seguro = 30.91
 const comisionProductor = 0.065
+const zonaResidencia = ["", "Ciudad/área urbana", "Conurbano", "Pequeña ciudad/pueblo"]
+zonaResidencia.push("Barrio privado")
